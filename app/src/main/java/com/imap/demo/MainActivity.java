9814151a15af3fcb1d01b421959ee58a108c6348
zxy94400 <zxy94400@alibaba-inc.com>
2016-07-29 11:07:03 +0800

    使用MapViewAdapter显示地图

--- app/src/main/java/com/imap/demo/MainActivity.java
+++ app/src/main/java/com/imap/demo/MainActivity.java
@@ -4,13 +4,43 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import com.imap.R;
+import com.imap.maps.model.MapViewAdapter;
 
 
 public class MainActivity extends Activity {
+
+    MapViewAdapter mapViewAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mapViewAdapter = (MapViewAdapter) findViewById(R.id.map);
+        mapViewAdapter.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapViewAdapter.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapViewAdapter.onPause();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapViewAdapter.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapViewAdapter.onDestroy();
     }
 }
